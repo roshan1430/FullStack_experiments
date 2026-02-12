baa75752639c1958f68b7ebd6632c70c0dabcf05
Roshan Manoj Dod <142732867+roshan1430@users.noreply.github.com>
2026-02-12 10:36:17 +0530

    Add files via upload

---
+++ Exp1/main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Feature Update Added Successfully.");
+
+
+        displayStatus();
+    }
+
+    public static void displayStatus() {
+        System.out.println("Initial version of the project is running successfully.");
+    }
+}
